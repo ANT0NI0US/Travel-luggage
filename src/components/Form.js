3eac090545a4329ab5d511e19eb122579f0faec7
AntoniousNasr <React.3@smartgate-egypt.com>
2024-10-15 08:45:55 -0700

    enhance performance

--- src/components/Form.js
+++ src/components/Form.js
@@ -19,7 +19,7 @@ export default function Form({ onAddItems }) {
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your 😍 trip?</h3>
+      <h2>What do you need for your 😍 trip?</h2>
       <select
         value={quantity}
         onChange={(e) => setQuantity(Number(e.target.value))}
